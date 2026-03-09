da4ae5fa871cde6507f027e971f4f5a304053047
itxzmevishal <sanapvishal128@gmail.com>
2026-03-09 19:26:43 +0530

    initial commit

---
+++ src/components/About.tsx
@@ -0,0 +1,16 @@
+import "./styles/About.css";
+
+const About = () => {
+  return (
+    <div className="about-section" id="about">
+      <div className="about-me">
+        <h3 className="title">About Me</h3>
+        <p className="para">
+           Computer Engineering student experienced in building full stack web applications using React, TypeScript, AngularJS, Node.js, Express.js, and MySQL. Also experienced in testing and managing APIs using Postman API tool for verifying requests, responses, and API endpoints. Skilled in developing responsive frontends, RESTful APIs, backend logic, database management, and version control using Git and GitHub. 
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
